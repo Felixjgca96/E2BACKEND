63ce147e29530fe38ac78e04216ac81af0c1029e
felix <felixjgca@gmail.com>
2023-11-15 01:49:07 -0300

    modificacion en bd

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async (): Promise<void> => {
     try {
-        await mongoose.connect('mongodb+srv://felixjgca:Fjgca96*@cluster0.krzalrv.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://felixjgca:EdXl1QAh59kcOXWz@cluster0.krzalrv.mongodb.net/?retryWrites=true&w=majority')
         console.log('Online database')
     } catch (error) {
         console.log(error);
